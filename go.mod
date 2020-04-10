f812b972e61b3b798b273b131b959aca248b509d
thisisaaronland <devnull@localhost>
2020-04-10 15:34:54 -0700

    prove it works with reader thingy

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/sfomuseum/go-www-geotag-whosonfirst
 go 1.12
 
 require (
-	github.com/sfomuseum/go-www-geotag v0.0.0-20200410184804-adea944aaf7a
+	github.com/sfomuseum/go-www-geotag v0.0.0-20200410223247-32220fdccf66
+	github.com/tidwall/gjson v1.6.0
 	github.com/tidwall/sjson v1.1.1
+	github.com/tomtaylor/go-whosonfirst-format v0.2.0
+	github.com/whosonfirst/go-reader v0.1.1
 	github.com/whosonfirst/go-whosonfirst-uri v0.2.0
 	github.com/whosonfirst/go-writer v0.1.0
 )
--- writer/whosonfirst.go
+++ writer/whosonfirst.go
@@ -5,28 +5,36 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/sfomuseum/go-www-geotag/geojson"
 	geotag_writer "github.com/sfomuseum/go-www-geotag/writer"
+	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
+	"github.com/tomtaylor/go-whosonfirst-format"
 	wof_uri "github.com/whosonfirst/go-whosonfirst-uri"
-	"github.com/whosonfirst/go-writer"	
+	"github.com/whosonfirst/go-writer"
+	"github.com/whosonfirst/go-reader"	
 	"io/ioutil"
+	"log"
 	"net/url"
 )
 
-const ALT_LABEL string = "geotag"
+const GEOTAG_NS string = "geotag"
+const GEOTAG_SRC string = "geotag"
+const GEOTAG_LABEL string = "geotag"
 
 func init() {
 	ctx := context.Background()
-	geotag_writer.RegisterWriter(ctx, "whosonfirst", NewWhosOnFirstWriter)
+	geotag_writer.RegisterWriter(ctx, "whosonfirst", NewWhosOnFirstGeotagWriter)
 }
 
-type WhosOnFirstWriter struct {
+type WhosOnFirstGeotagWriter struct {
 	geotag_writer.Writer
 	writer writer.Writer
+	reader reader.Reader	
 }
 
-func NewWhosOnFirstWriter(ctx context.Context, uri string) (geotag_writer.Writer, error) {
+func NewWhosOnFirstGeotagWriter(ctx context.Context, uri string) (geotag_writer.Writer, error) {
 
 	u, err := url.Parse(uri)
 
@@ -36,63 +44,124 @@ func NewWhosOnFirstWriter(ctx context.Context, uri string) (geotag_writer.Writer
 
 	q := u.Query()
 
-	wof_wr_uri := q.Get("writer")
+	log.Println(q)
 
-	if wof_wr_uri == "" {
+	writer_uri := q.Get("writer")
+	reader_uri := q.Get("reader")	
+
+	if writer_uri == "" {
 		return nil, errors.New("Missing writer parameter")
 	}
-	
-	wof_wr, err := writer.NewWriter(ctx, wof_wr_uri)
+
+	wof_wr, err := writer.NewWriter(ctx, writer_uri)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if reader_uri == "" {
+		return nil, errors.New("Missing reader parameter")
+	}
+
+	wof_rd, err := reader.NewReader(ctx, reader_uri)
 
 	if err != nil {
 		return nil, err
 	}
 	
-	wr := &WhosOnFirstWriter{
+	wr := &WhosOnFirstGeotagWriter{
 		writer: wof_wr,
+		reader: wof_rd,
 	}
-	
+
 	return wr, nil
 }
 
-func (wr *WhosOnFirstWriter) WriteFeature(ctx context.Context, uri string, f *geojson.GeotagFeature) error {
+func (wr *WhosOnFirstGeotagWriter) WriteFeature(ctx context.Context, uri string, geotag_f *geojson.GeotagFeature) error {
 
-	id, _, err := wof_uri.ParseURI(uri)
+	uri = "1511948897"
+
+	id, uri_args, err := wof_uri.ParseURI(uri)
+
+	if err != nil {
+		return err
+	}
+
+	if uri_args.IsAlternate {
+		return errors.New("Alt files are not supported yet")
+	}
+	
+	rel_path, err := wof_uri.Id2RelPath(id)
+
+	if err != nil {
+		return err
+	}
+
+	main_fh, err := wr.reader.Read(ctx, rel_path)
+
+	if err != nil {
+		return err
+	}
+
+	main_body, err := ioutil.ReadAll(main_fh)
+
+	if err != nil {
+		return err
+	}
+
+	repo_rsp := gjson.GetBytes(main_body, "properties.wof:repo")
+
+	if !repo_rsp.Exists(){
+		return errors.New("Missing wof:repo")
+	}
+
+	main_repo := repo_rsp.String()
+	
+	geotag_body, err := json.Marshal(geotag_f)
+
+	if err != nil {
+		return err
+	}
+
+	alt_body, err := GeotagFeatureToAltFeature(ctx, uri, geotag_body)
+
+	if err != nil {
+		log.Println("SAD", err)
+		return err
+	}
+
+	alt_body, err = sjson.SetBytes(alt_body, "properties.wof:repo", main_repo)
 
 	if err != nil {
 		return err
 	}
 	
-	body, err := json.Marshal(f)
-
-	if err != nil {
-		return err
-	}
-
-	alt_body, err := GeotagFeatureToAltFeature(ctx, uri, body)
+	alt_body, err = Format(alt_body)
 
 	if err != nil {
 		return err
 	}
 
 	alt_geom := &wof_uri.AltGeom{
-		Source:   ALT_LABEL,
+		Source: GEOTAG_LABEL,
 	}
 
-	uri_args := &wof_uri.URIArgs{
+	alt_args := &wof_uri.URIArgs{
 		IsAlternate: true,
 		AltGeom:     alt_geom,
 	}
-	
-	alt_uri, err := wof_uri.Id2Fname(id, uri_args)
+
+	alt_uri, err := wof_uri.Id2Fname(id, alt_args)
 
 	if err != nil {
 		return err
 	}
+
+	log.Println("ALT URI", alt_uri)
 	
 	br := bytes.NewReader(alt_body)
 	fh := ioutil.NopCloser(br)
-	
+
 	return wr.writer.Write(ctx, alt_uri, fh)
 }
 
@@ -102,6 +171,37 @@ func GeotagFeatureToAltFeature(ctx context.Context, uri string, body []byte) ([]
 
 	if err != nil {
 		return nil, err
+	}
+
+	to_ns := []string{
+		"angle",
+		"bearing",
+		"distance",
+	}
+
+	for _, k := range to_ns {
+
+		path_old := fmt.Sprintf("properties.%s", k)
+		path_ns := fmt.Sprintf("properties.%s:%s", GEOTAG_NS, k)
+
+		rsp := gjson.GetBytes(body, path_old)
+
+		if !rsp.Exists() {
+			msg := fmt.Sprintf("Missing '%s' property", path_old)
+			return nil, errors.New(msg)
+		}
+
+		body, err = sjson.DeleteBytes(body, path_old)
+
+		if err != nil {
+			return nil, err
+		}
+
+		body, err = sjson.SetBytes(body, path_ns, rsp.Float())
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	body, err = sjson.SetBytes(body, "id", id)
@@ -116,13 +216,37 @@ func GeotagFeatureToAltFeature(ctx context.Context, uri string, body []byte) ([]
 		return nil, err
 	}
 
-	body, err = sjson.SetBytes(body, "properties.src:alt_label", ALT_LABEL)
+	body, err = sjson.SetBytes(body, "properties.src:alt_label", GEOTAG_LABEL)
 
 	if err != nil {
 		return nil, err
 	}
 
-	body, err = sjson.SetBytes(body, "properties.src:geom", "geotag")
+	body, err = sjson.SetBytes(body, "properties.src:geom", GEOTAG_SRC)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func Format(feature []byte) ([]byte, error) {
+
+	// TODO: Add FormatBytes to go-whosonfirst-feature
+	
+	var f format.Feature
+	json.Unmarshal(feature, &f)
+	
+	body, err := format.FormatFeature(&f)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Add omitempty hooks for bbox in go-whosonfirst-feature
+	
+	body, err = sjson.DeleteBytes(body, "bbox")
 
 	if err != nil {
 		return nil, err
